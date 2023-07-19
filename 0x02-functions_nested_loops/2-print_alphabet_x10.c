9107c1b37bb5a122c50ed5ab251fa776d72cd9b2
PETHORG <peterolubowale410@gmail.com>
2023-07-18 21:40:36 -0700

    C, functions and nested loop project

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in ten lines
+ *
+ * Return: Always (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+
+	return (0);
+}
