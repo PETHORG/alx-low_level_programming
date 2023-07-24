19ea17eebdd60ea97c62e6f047edd2346dd6a3a7
PETHORG <peterolubowale410@gmail.com>
2023-07-24 12:08:59 -0700

    C, pointer, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,40 @@
 #include "main.h"
+
 /**
- * rev_string - reverses string
- * @s: pointer to a char
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
  */
 
 void rev_string(char *s)
 {
-	int tmp, beg = 0;
-	int end = n - 1; /* omit null terminator in length */
+	int i = 0;
+	int j = (_strlen(s) - 1);
+	char tmp;
 
-	while (beg < end)
+	while (i < j)
 	{
-		tmp = *(a + beg);
-		*(a + beg) = *(a + end);
-		*(a + end) = tmp;
-		beg++, end--;
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i++, j--;
 	}
 }
