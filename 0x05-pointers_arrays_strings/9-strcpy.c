c48e198101fbfdd69080544cad7c8e5a501e614b
PETHORG <peterolubowale410@gmail.com>
2023-07-24 21:48:22 -0700

    C, pointer, arrays and strings project

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,21 +1,37 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed by src to the bufferof dest
- * @dest: pointer to be copied to
- * @src: pointer that holds the string
- *
- * Return: returns the pointer dest
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; *src; i++)
-	{
+	for (i = 0; i <= _strlen(src); i++)
 		dest[i] = src[i];
-	}
-
-	return (dest);
 }
