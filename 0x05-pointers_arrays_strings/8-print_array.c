4018197f75464381c0274bf76e7cb424a3391e1c
PETHORG <peterolubowale410@gmail.com>
2023-07-24 20:15:59 -0700

    C, pointer, arrays and strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,9 @@ void print_array(int *a, int n)
 
 	if (n >= 0)
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			if (i < n)
+			if (i < (n - 1))
 			{
 				printf("%d, ", a[i]);
 			}
