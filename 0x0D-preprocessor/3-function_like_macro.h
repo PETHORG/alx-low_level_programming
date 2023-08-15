4d48504290fb99f273f8f12e1e241da51c276054
PETHORG <peterolubowale410@gmail.com>
2023-08-15 08:14:38 -0700

    C-preprocessor, macros

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) {
-	if (x < 0)
-		return x * (-1);
-	else
-		return x * 1;
-}
+#define ABS(x) (x > 0) ? (x * 1) : (x * (-1))
 
 #endif /*FUNCTION_LIKE_MACRO_H*/
 
