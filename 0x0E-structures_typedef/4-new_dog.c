c9a0fdaaac7f7126c531059bd662b497a09730ec
PETHORG <peterolubowale410@gmail.com>
2023-08-17 02:18:52 -0700

    C,structures and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,63 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
- * @name: name of the new dog
- * @age: age of the new dog
- * @owner: owners name
- *
- * Return: typedef dog_t of struct dog
+ * len - find length of string
+ * @str: string
+ * Return: length
  */
+int len(char *str)
+{
+	int i;
 
+	for (i = 0; *(str + i); i++)
+		;
+	return (i);
+}
+/**
+ * strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
+ */
+char *strcpy(char *cpy, char *src)
+{
+	int i;
+
+	for (i = 0; i <= len(src); i++)
+		cpy[i] = src[i];
+	return (cpy);
+}
+/**
+ * new_dog - create new instance of struct dog
+ * @name: member
+ * @age: member
+ * @owner: member
+ * Return: initialized instance of struct dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog_cpy;
-	char *cpy_of_name;
-	char *cpy_of_owner;
+	dog_t *dog1;
+	char *copy_name;
+	char *copy_owner;
 
-	dog_cpy = malloc(sizeof(dog_t));
-	if (dog_cpy == NULL)
+	dog1 = malloc(sizeof(dog_t)); /* validate if dog1 initiated correctly */
+	if (dog1 == NULL)
 		return (NULL);
 
-	dog_cpy->age = age;
+	dog1->age = age;
 
-
+	/* make copies of struct members and validate, else free on error */
+	/* set values of struct members to copies of arguments or set to NULL */
 	if (name != NULL)
 	{
-		cpy_of_name = malloc(strlen(name) + 1);
-		
-		/*check memory allocation worked*/
-		if (cpy_of_name == NULL)
+		copy_name = malloc(len(name) + 1);
+		if (copy_name == NULL)
 		{
-			free(dog_cpy);
+			free(dog1);
 			return (NULL);
 		}
-		dog_cpy->name = strcpy(cpy_of_name, name);
+		dog1->name = strcpy(copy_name, name);
 	}
 	else
-	{
-		dog_cpy->name = NULL;
-	}
+		dog1->name = NULL;
 
 	if (owner != NULL)
 	{
-		cpy_of_owner = malloc(strlen(owner) + 1);
-		
-		/*check memory allocation worked*/
-		if (cpy_of_owner == NULL)
+		copy_owner = malloc(len(owner) + 1);
+		if (copy_owner == NULL)
 		{
-			free(dog_cpy);
+			free(copy_name);
+			free(dog1);
 			return (NULL);
 		}
-		dog_cpy->owner = strcpy(cpy_of_owner, owner);
+		dog1->owner = strcpy(copy_owner, owner);
 	}
 	else
-	{
-		dog_cpy->owner = NULL;
-	}
+		dog1->owner = NULL;
 
-	return (dog_cpy);
+	return (dog1);
 }
