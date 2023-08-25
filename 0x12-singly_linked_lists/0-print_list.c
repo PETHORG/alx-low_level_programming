506c2b8390e0957b7503eb3ddac162f2798889ca
PETHORG <peterolubowale410@gmail.com>
2023-08-24 19:25:20 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_list - prints all elements of list_t list
+ * @h: struct passed as parameter
+ *
+ * Return: size_t(unsigned int)
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	list_t *pt;
+	pt = h;
+
+	while (pt != NULL)
+	{
+		if (pt->str == NULL)
+			printf("[0] (nil)");
+
+		printf("[%u] %s\n", pt->len, pt->str);
+		pt = pt->next;
+		i++;
+	}
+
+	return (i);
+}
