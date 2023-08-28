f498f8163202b7a7ce56999487e020a479e1ad0c
PETHORG <peterolubowale410@gmail.com>
2023-08-28 15:34:33 -0700

    C- more singly linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees list
+ * @head: head of linked list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *h;
+	h = head;
+
+	if (h == NULL)
+		return;
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		free(h);
+	}
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list
+ * @head: head of linked list
+ */
+
+void free_listint2(listint_t *head)
+{
+	listint_t *h;
+	h = head;
+
+	if (h == NULL)
+		return;
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		free(h);
+	}
+
+	h == NULL;
+}
